92c426c01ec0d83f1a4ba627673bc12fe748c602
Jaseem <muhammedjaseem753@gmail.com>
2025-08-13 16:01:34 +0530

    site updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,18 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="twitter-conversion-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','qbsq3');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <noscript>
